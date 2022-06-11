5844c63caa2c9ffed8fe71a2010f95b83f02d809
linhgiac <khanhly.hcmut@gmail.com>
2022-06-11 12:26:20 +0700

    Update footer

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import Page from "./pages/pages";
 import Header from "./components/header/header";
+import Footer from "./components/footer/footer";
 
-import {BrowserRouter} from "react-router-dom"
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
     return (
-        <> 
-        <BrowserRouter>
-          {(window.location.pathname !== '/login' && window.location.pathname !== '/signup') && <Header />}
-        
-          <Page />
-        </BrowserRouter>
+        <>
+            <BrowserRouter>
+                {window.location.pathname !== "/login" &&
+                    window.location.pathname !== "/signup" && <Header />}
+
+                <Page />
+                {window.location.pathname !== "/login" &&
+                    window.location.pathname !== "/signup" && <Footer />}
+            </BrowserRouter>
         </>
-               
-        
     );
 }
 
