11741e2da47eb63d72856997baabea76c1d99a9a
deathkun26 <pdminhtoan@gmail.com>
2022-06-16 12:53:06 +0700

    Map Data (no DB)

--- src/pages/news/news.js
+++ src/pages/news/news.js
@@ -63,25 +63,34 @@ const NewsMainContent = () => {
     ];
     const fakeData = [
         {
+            id: 1,
+            imgSrc: "http://zitga.com.vn/wp-content/uploads/2022/05/11-1.jpg",
+            title: "[OUR BELOVED SPRING] CHUYẾN ĐI DÀNH CHO NHỮNG TÂM HỒN RỰC CHÁY",
+            date: `${month[current.getMonth()]} ${current.getDate()}, ${current.getFullYear()}`,
+        },
+        {
+            id: 2,
             imgSrc: "http://zitga.com.vn/wp-content/uploads/2022/05/11-1.jpg",
             title: "[OUR BELOVED SUMMER] CHUYẾN ĐI DÀNH CHO NHỮNG TÂM HỒN RỰC CHÁY",
             date: `${month[current.getMonth()]} ${current.getDate()}, ${current.getFullYear()}`,
         },
+        {
+            id: 3,
+            imgSrc: "http://zitga.com.vn/wp-content/uploads/2022/05/11-1.jpg",
+            title: "[OUR BELOVED AUTUMN] CHUYẾN ĐI DÀNH CHO NHỮNG TÂM HỒN RỰC CHÁY",
+            date: `${month[current.getMonth()]} ${current.getDate()}, ${current.getFullYear()}`,
+        },
+        {
+            id: 4,
+            imgSrc: "http://zitga.com.vn/wp-content/uploads/2022/05/11-1.jpg",
+            title: "[OUR BELOVED WINTER] CHUYẾN ĐI DÀNH CHO NHỮNG TÂM HỒN RỰC CHÁY",
+            date: `${month[current.getMonth()]} ${current.getDate()}, ${current.getFullYear()}`,
+        },
     ];
-    const [count, setCount] = useState(0);
-    const [isLike, setIsLike] = useState(false);
-    const handleLiked = () => {
-        setIsLike(!isLike);
-        if (isLike) {
-            setCount(count - 1);
-        } else {
-            setCount(count + 1);
-        }
-    };
 
     return (
         <div className="news-main-content-container">
-            <div className="news-image-title">
+            {/* <div className="news-image-title">
                 <a href="#">
                     <img src={fakeData[0].imgSrc} />
                 </a>
@@ -113,9 +122,71 @@ const NewsMainContent = () => {
                         </div>
                     </div>
                 </div>
-            </div>
+            </div> */}
+            <NewsList newsData={fakeData} />
         </div>
     );
 };
+
+const NewsList = ({ newsData }) => {
+    const newsList = newsData.map((news) =>
+        <NewsContainer key={news.id.toString()} news={news} />
+    );
+    return (
+        <>{newsList}</>
+    )
+}
+
+const NewsContainer = ({ news }) => {
+    const [count, setCount] = useState(0);
+    const [isLike, setIsLike] = useState(false);
+    const handleLiked = () => {
+        setIsLike(!isLike);
+        if (isLike) {
+            setCount(count - 1);
+        } else {
+            setCount(count + 1);
+        }
+    };
+    return (
+        <>
+            <div className="news-image-title">
+                <a href="#">
+                    <img src={news.imgSrc} />
+                </a>
+            </div>
+            <div className="news-main-content-inner">
+                <div className="news-main-content-pre-title">
+                    <div className="news-pre-title-btn">tin tức</div>
+                </div>
+                <div className="news-main-content-title">
+                    <a href="/news/news-details-01">{news.title}</a>
+                </div>
+                <div className="news-main-content-post-tiltle">
+                    <div className="news-post-left">
+                        <NavLink className="news-post-left-element" to="#">
+                            {news.date}
+                        </NavLink>
+
+                        <div className="news-post-left-like">
+                            <HeartFilled
+                                onClick={handleLiked}
+                                style={{ color: "#ff0e1f" }}
+                            />
+                            <span>{count}</span>
+                        </div>
+                    </div>
+                    <div className="news-post-right">
+                        <div className="news-post-right-element">
+                            by admin
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+
 
 export default News;
