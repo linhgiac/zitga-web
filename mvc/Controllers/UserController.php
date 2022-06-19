830aaad60a186d1b4a08e69fd9c4c646cd60e108
ThaiTran31 <thaitrancs31@gmail.com>
2022-06-19 11:10:37 +0700

    fix separated path

--- mvc/Controllers/UserController.php
+++ mvc/Controllers/UserController.php
@@ -127,7 +127,7 @@ class UserController extends BaseController
                     $response = array(
                         "success" => true,
                         "message" => "Image has uploaded",
-                        "avatar" => dirname(dirname(__FILE__)) . $DIR . "/" . $UPLOAD_IMG_NAME
+                        "avatar" => dirname(dirname(__FILE__)) . $DIR . "\\\\" . $UPLOAD_IMG_NAME
                     );
                 } else {
                     $response = array(
