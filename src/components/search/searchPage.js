9dc46acab584e3a11d86e7bac52ea838f1fde119
linhgiac <khanhly.hcmut@gmail.com>
2022-06-19 09:37:16 +0700

    Update SearchPage

--- src/components/search/searchPage.js
+++ src/components/search/searchPage.js
@@ -1,9 +1,9 @@
 import React from "react";
-import "./searchPage.css"
+import "./searchPage.css";
 
 const SearchPage = () => {
     const fakeDataSearch = [{ search: "test" }];
-    const fakeDataResults = [{ title: "result1" }, { title: "result2" }];
+    const fakeDataResults = [{ title: "result1" }, { title: "result2" },  { title: "result3" },  { title: "result3" }];
     return (
         <div className="search-page-container">
             <div className="search-page-title">
@@ -11,12 +11,25 @@ const SearchPage = () => {
             </div>
             <div className="search-page-results">
                 <div className="search-page-result-item">
-                  <a href="#" className="search-page-result-link">
-                    {fakeDataResults[0].title}
-                  </a>
-                  <a href="#" className="search-page-result-link">
-                    {fakeDataResults[1].title}
-                  </a>
+                    <div className="search-page-result-page">
+                        <p>News</p>
+                        <div className="search-page-result-link">
+                            <a href="#">{fakeDataResults[0].title}</a>
+                        </div>
+                        <div className="search-page-result-link">
+                            <a href="#">{fakeDataResults[1].title}</a>
+                        </div>
+                    </div>
+
+                    <div className="search-page-result-page">
+                        <p>Careers</p>
+                        <div className="search-page-result-link">
+                            <a href="#">{fakeDataResults[2].title}</a>
+                        </div>
+                        <div className="search-page-result-link">
+                            <a href="#">{fakeDataResults[3].title}</a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
